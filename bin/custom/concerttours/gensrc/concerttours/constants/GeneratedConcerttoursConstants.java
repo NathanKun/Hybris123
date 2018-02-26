dce2a5f063e69dc795341809c2532e73a3490a2a
HE Junyang <nathanhejunyang@gmail.com>
2018-02-26 13:20:40 +0100

    Add your Extension

---
+++ bin/custom/concerttours/gensrc/concerttours/constants/GeneratedConcerttoursConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 26 févr. 2018 13:19:14                      ---
+ * ----------------------------------------------------------------
+ */
+package concerttours.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedConcerttoursConstants
+{
+	public static final String EXTENSIONNAME = "concerttours";
+	
+	protected GeneratedConcerttoursConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
--- bin/platform/bootstrap/gensrc/de/hybris/platform/core/model/type/AtomicTypeModel.java
+++ bin/platform/bootstrap/gensrc/de/hybris/platform/core/model/type/AtomicTypeModel.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at 26 févr. 2018 13:03:24                      ---
+ * --- Generated at 26 févr. 2018 13:19:14                      ---
  * ----------------------------------------------------------------
  *  
  * [y] hybris Platform
