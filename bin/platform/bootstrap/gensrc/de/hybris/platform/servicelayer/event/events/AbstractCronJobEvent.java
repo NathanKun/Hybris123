a0f1ec872ba4b7efc40269825df171ebb998d650
HE Junyang <nathanhejunyang@gmail.com>
2018-02-27 14:49:36 +0100

    Create a Listener
    Former-commit-id: 562731705ae8293b8635f9c417724db074f7ab78

--- bin/platform/bootstrap/gensrc/de/hybris/platform/servicelayer/event/events/AbstractCronJobEvent.java
+++ bin/platform/bootstrap/gensrc/de/hybris/platform/servicelayer/event/events/AbstractCronJobEvent.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN!
- * --- Generated at 27 févr. 2018 14:34:27
+ * --- Generated at 27 févr. 2018 14:43:48
  * ----------------------------------------------------------------
  *
  * [y] hybris Platform
--- bin/platform/bootstrap/gensrc/de/hybris/platform/servicelayer/event/events/AfterCronJobFinishedEvent.java
+++ bin/platform/bootstrap/gensrc/de/hybris/platform/servicelayer/event/events/AfterCronJobFinishedEvent.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN!
- * --- Generated at 27 févr. 2018 14:34:28
+ * --- Generated at 27 févr. 2018 14:43:48
  * ----------------------------------------------------------------
  *
  * [y] hybris Platform
